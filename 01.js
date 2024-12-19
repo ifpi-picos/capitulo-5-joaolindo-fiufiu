9dea037342864f43538eb8f275341ad84c7ac6fb
Joao <juaohehehe528@gmail.com>
2024-12-19 15:40:36 -0300

    questao 1, 2 ,3

---
+++ 01.js
@@ -0,0 +1,6 @@
+const nmr=Number(prompt("Escreva seu número: "))
+if (nmr % 2 === 0){
+    console.log("Esse número é par");
+}else{
+    console.log("Esse número é impar");
+}
---
+++ 02.js
@@ -0,0 +1,11 @@
+const nm1=Number(prompt("Escreva seu número: "))
+const nm2=Number(prompt("Escreva seu número: "))
+const nm3=Number(prompt("Escreva seu número: "))
+mai = (nm1, nm2, nm3)
+if (nm1 > mai){
+console.log("O ", nm1, "é maior");
+}if  (nm2> mai){
+    console.log("O", nm2, "é maior")
+}if (nm3> mai){
+    console.log("O ", nm3,"é maior")
+}
